b8753ab4333176ebf003d3851877bb4bcc420283
thaboChino <thabo.boysen376@gmail.com>
2023-11-29 10:22:00 +0200

    must solve errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        NavbarComponent
+        
+    ],
+
+        // provider used to create fake backe
+        providers: [
+            // provider used to create fake backend
+            fakestoreapi
+        ],   
+      
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
