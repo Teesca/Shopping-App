cfa432f745282ce017365fde01387ccc37462df5
Intel_T <63907409+Ntalapro@users.noreply.github.com>
2023-12-30 23:12:19 +0200

    .

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { NgIf } from '@angular/common';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { UserData } from '../item-details/cartInterface';
-import { Subject, count, takeUntil } from 'rxjs';
+import { filter } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -16,55 +14,15 @@ import { Subject, count, takeUntil } from 'rxjs';
 })
 export class NavbarComponent {
 
-  loggedin: boolean = false;
-  allproductsFromCart: any[] = [];
-  total: number = 0;
-  private unsubscribe$ = new Subject<void>();
-
-  constructor(
-    private auth: AngularFireAuth,
-    private firestore: AngularFirestore,
-    private router: Router
-  ) {}
+  loggedin: boolean = false; 
+  constructor(private auth: AngularFireAuth, private router: Router) {}
 
   ngOnInit() {
-    this.auth.authState.pipe(takeUntil(this.unsubscribe$)).subscribe((user) => {
-      this.loggedin = !!user;
-      this.cartCount();
+    this.auth.authState.subscribe(user => {
+      this.loggedin = !!user; // Set to true if the user is logged in, otherwise false
     });
   }
 
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-
-  cartCount() {
-    this.auth.authState.pipe(takeUntil(this.unsubscribe$)).subscribe((user) => {
-      if (user !== null) {
-        const userDocRef = this.firestore.collection('users').doc(user.uid);
-
-        // Use Firestore transaction to get the cart array
-        this.firestore
-          .firestore.runTransaction((transaction) => {
-            return transaction.get(userDocRef.ref).then((userDoc) => {
-              if (!userDoc.exists) {
-                throw new Error('User document does not exist!');
-              }
-              const userData = userDoc.data() as UserData;
-              this.allproductsFromCart = userData.cart || [];
-              this.total = this.allproductsFromCart.length; // Update the total count
-            });
-          })
-          .then(() => {
-            console.log('Cart count updated successfully');
-          })
-          .catch((error) => {
-            console.error('Error updating cart count:', error);
-          });
-      }
-    });
-  }
   //signout user when the sign out button is clicked
   logout() {
     this.auth.signOut()
