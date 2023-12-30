34a6b9bff544329bf13f0b92e0cc0a455c2dd98b
Tshepiso Ncosane <63907409+Ntalapro@users.noreply.github.com>
2023-12-30 23:19:06 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ import { UserprofileComponent } from './userprofile/userprofile.component'
     NavbarComponent,
     FooterComponent,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyDwc5VdDjdwedqedSkh8td8R3KHY8jkf1CgdyUls",
+      apiKey: "AIzaSyDwc5VdDjdwed3KHY8jkf1CgdyUls",
       authDomain: "the-can-do.firebaseapp.com",
       projectId: "the-can-do",
       storageBucket: "the-can-do.appspot.com",
